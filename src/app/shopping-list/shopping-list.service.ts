88059dba42e182f0862c0dab8d789681104f34b8
thereallucas98 <lucas1998david@gmail.com>
2021-12-30 11:38:48 -0300

    (feat): Adding the Shopping List Service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
+import { Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
+@Injectable()
 export class ShoppingListService {
-  
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
