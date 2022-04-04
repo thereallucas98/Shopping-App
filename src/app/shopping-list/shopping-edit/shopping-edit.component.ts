947fd0b4ddb19058cb2b48d097f6b647adbc8cb3
thereallucas98 <lucas1998david@gmail.com>
2022-04-04 17:27:58 -0300

    Updating existing Items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -35,9 +35,15 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onAddItem(form: NgForm) {
-    const value = form.value;
-    const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+    if (this.editMode) {
+      const value = form.value;
+      const newIngredient = new Ingredient(value.name, value.amount);
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      const value = form.value;
+      const newIngredient = new Ingredient(value.name, value.amount);
+      this.slService.addIngredient(newIngredient);
+    }
   }
 
   ngOnDestroy(): void {
